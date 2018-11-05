02dde3526a72380383909646a3e6c61530579f20
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2018-11-05 14:06:05 +0100

    Callback connector

--- include/bitonic/simd/int32.hpp
+++ include/bitonic/simd/int32.hpp
@@ -6,6 +6,7 @@
 #include <cstdint>
 
 #include <ostream>
+#include <iomanip>
 #include <string>
 
 namespace Bitonic {
@@ -34,6 +35,26 @@ struct Int32Base {
         return _mm256_blend_epi32(a, b, N);
     }
 
+    static simd_type index_sequence() {
+        return _mm256_setr_epi32(0,1,2,3,4,5,6,7);
+    }
+
+    static simd_type shift_left(simd_type v, int i) {
+        return _mm256_slli_epi32(v, i);
+    }
+
+    static simd_type bitwise_or(simd_type a, simd_type b) {
+        return _mm256_or_si256(a, b);
+    }
+
+    static simd_type bitwise_and(simd_type a, simd_type b) {
+        return _mm256_and_si256(a, b);
+    }
+
+    static simd_type add(simd_type a, simd_type b) {
+        return _mm256_add_epi32(a, b);
+    }
+
     template<bool kAligned, bool kStream>
     static simd_type load(const simd_type *it) {
         static_assert(!(kStream && !kAligned), "Streaming stores require aligned access");
@@ -49,9 +70,27 @@ struct Int32Base {
         }
     }
 
+    template <typename Callback>
+    static simd_type load_callback(Callback&& cb) {
+        return _mm256_setr_epi32(cb(0), cb(1), cb(2), cb(3), cb(4), cb(5), cb(6), cb(7));
+    }
+
+    template <typename Callback, typename value_type>
+    static simd_type partial_load_callback(Callback&& cb, int len, const value_type empty_value) {
+        return _mm256_setr_epi32(
+                       cb(0)              ,
+            len > 1 ? cb(1) : empty_value,
+            len > 2 ? cb(2) : empty_value,
+            len > 3 ? cb(3) : empty_value,
+            len > 4 ? cb(4) : empty_value,
+            len > 5 ? cb(5) : empty_value,
+            len > 6 ? cb(6) : empty_value,
+            len > 7 ? cb(7) : empty_value);
+    }
+
     template<typename value_type>
     static simd_type partial_load(const simd_type *begin, const size_t length, const value_type empty_value) {
-        const simd_type index = _mm256_setr_epi32(0,1,2,3,4,5,6,7);
+        const simd_type index = index_sequence();
         const simd_type len = _mm256_set1_epi32(length);
         auto mask = _mm256_cmpgt_epi32(len, index); // gives 1 where to load
         auto value = _mm256_maskload_epi32(reinterpret_cast<const int*>(begin), mask);
@@ -89,20 +128,54 @@ struct Int32Base {
         _mm256_maskstore_epi32(reinterpret_cast<int*>(begin), mask, x);
     };
 
+
+    template <typename Callback>
+    static void store_callback(Callback&& cb, simd_type x) {
+        cb(0, _mm256_extract_epi32(x, 0));
+        cb(1, _mm256_extract_epi32(x, 1));
+        cb(2, _mm256_extract_epi32(x, 2));
+        cb(3, _mm256_extract_epi32(x, 3));
+
+        cb(4, _mm256_extract_epi32(x, 4));
+        cb(5, _mm256_extract_epi32(x, 5));
+        cb(6, _mm256_extract_epi32(x, 6));
+        cb(7, _mm256_extract_epi32(x, 7));
+    }
+
+    template <typename Callback>
+    static void partial_store_callback(Callback&& cb, int len, const simd_type x) {
+                     cb(0, _mm256_extract_epi32(x, 0));
+        if (len > 1) cb(1, _mm256_extract_epi32(x, 1)); else return;
+        if (len > 2) cb(2, _mm256_extract_epi32(x, 2)); else return;
+        if (len > 3) cb(3, _mm256_extract_epi32(x, 3)); else return;
+        if (len > 4) cb(4, _mm256_extract_epi32(x, 4)); else return;
+        if (len > 5) cb(5, _mm256_extract_epi32(x, 5)); else return;
+        if (len > 6) cb(6, _mm256_extract_epi32(x, 6)); else return;
+        if (len > 7) cb(7, _mm256_extract_epi32(x, 7)); else return;
+    }
+
+
+
     static void print(const simd_type x, std::ostream& os = std::cout) {
-        os << _mm256_extract_epi32(x, 0) << " "
-           << _mm256_extract_epi32(x, 1) << " "
-           << _mm256_extract_epi32(x, 2) << " "
-           << _mm256_extract_epi32(x, 3) << " "
-           << _mm256_extract_epi32(x, 4) << " "
-           << _mm256_extract_epi32(x, 5) << " "
-           << _mm256_extract_epi32(x, 6) << " "
-           << _mm256_extract_epi32(x, 7);
+        std::stringstream ss;
+        ss << std::hex << _mm256_extract_epi32(x, 0) << " "
+           << std::hex << _mm256_extract_epi32(x, 1) << " "
+           << std::hex << _mm256_extract_epi32(x, 2) << " "
+           << std::hex << _mm256_extract_epi32(x, 3) << " "
+           << std::hex << _mm256_extract_epi32(x, 4) << " "
+           << std::hex << _mm256_extract_epi32(x, 5) << " "
+           << std::hex << _mm256_extract_epi32(x, 6) << " "
+           << std::hex << _mm256_extract_epi32(x, 7);
+        os << ss.str();
     }
 };
 
 struct SignedInt32 : public Int32Base {
     using value_type = int32_t;
+
+    static simd_type broadcast(value_type a) {
+        return _mm256_set1_epi32(a);
+    }
 
     static simd_type min(simd_type a, simd_type b) {
         return _mm256_min_epi32(a, b);
@@ -117,6 +190,10 @@ struct SignedInt32 : public Int32Base {
 
 struct UnsignedInt32 : public Int32Base {
     using value_type = uint32_t;
+
+    static simd_type broadcast(value_type a) {
+        return _mm256_set1_epi32(a);
+    }
 
     static simd_type min(simd_type a, simd_type b) {
         return _mm256_min_epu32(a, b);
--- include/bitonic/simd/simd_sort.hpp
+++ include/bitonic/simd/simd_sort.hpp
@@ -5,6 +5,8 @@
 
 #include <limits>
 
+#include <tlx/meta.hpp>
+
 namespace Bitonic {
 
 template <typename SimdOps_>
@@ -57,6 +59,51 @@ public:
         }
     }
 
+
+    template <typename CallbackLoad, typename CallbackStore>
+    static void sort(CallbackLoad cbl, CallbackStore cbw, size_t n) {
+        const size_t regs = (n + SimdOps::kPacking  - 1) / SimdOps::kPacking;
+
+        switch(regs) {
+            case  1: sort_callback<1>(cbl, cbw, n); return;
+            case  2: sort_callback<2>(cbl, cbw, n); return;
+            case  3: sort_callback<3>(cbl, cbw, n); return;
+            case  4: sort_callback<4>(cbl, cbw, n); return;
+            case  5: sort_callback<5>(cbl, cbw, n); return;
+            case  6: sort_callback<6>(cbl, cbw, n); return;
+            case  7: sort_callback<7>(cbl, cbw, n); return;
+            case  8: sort_callback<8>(cbl, cbw, n); return;
+            case  9: sort_callback<9>(cbl, cbw, n); return;
+
+            case 10: sort_callback<10>(cbl, cbw, n); return;
+            case 11: sort_callback<11>(cbl, cbw, n); return;
+            case 12: sort_callback<12>(cbl, cbw, n); return;
+            case 13: sort_callback<13>(cbl, cbw, n); return;
+            case 14: sort_callback<14>(cbl, cbw, n); return;
+            case 15: sort_callback<15>(cbl, cbw, n); return;
+            case 16: sort_callback<16>(cbl, cbw, n); return;
+            case 17: sort_callback<17>(cbl, cbw, n); return;
+            case 18: sort_callback<18>(cbl, cbw, n); return;
+            case 19: sort_callback<19>(cbl, cbw, n); return;
+
+            case 20: sort_callback<20>(cbl, cbw, n); return;
+            case 21: sort_callback<21>(cbl, cbw, n); return;
+            case 22: sort_callback<22>(cbl, cbw, n); return;
+            case 23: sort_callback<23>(cbl, cbw, n); return;
+            case 24: sort_callback<24>(cbl, cbw, n); return;
+            case 25: sort_callback<25>(cbl, cbw, n); return;
+            case 26: sort_callback<26>(cbl, cbw, n); return;
+            case 27: sort_callback<27>(cbl, cbw, n); return;
+            case 28: sort_callback<28>(cbl, cbw, n); return;
+            case 29: sort_callback<29>(cbl, cbw, n); return;
+
+            case 30: sort_callback<30>(cbl, cbw, n); return;
+            case 31: sort_callback<31>(cbl, cbw, n); return;
+            case 32: sort_callback<32>(cbl, cbw, n); return;
+        }
+    }
+
+
     template <size_t k, bool kAligned, bool kStream>
     static void load_sort_store(value_type* begin, value_type* end) {
         auto packed_it = reinterpret_cast<simd_type*>(begin);
@@ -72,13 +119,87 @@ public:
             load<k-1, kAligned, kStream>(packed_it, registers);
         }
 
+
+        constexpr int shift = ::tlx::Log2<k * SimdOps::kPacking>::ceil;
+        {
+            auto index_seq = SimdOps::index_sequence();
+            const auto pack_bc = SimdOps::broadcast(SimdOps::kPacking);
+
+            tlx::call_for_range<0, k>([&](size_t idx) {
+                auto tmp = SimdOps::shift_left(registers[idx], shift);
+                tmp = SimdOps::bitwise_or(tmp, index_seq);
+                registers[idx] = tmp;
+
+                index_seq = SimdOps::add(index_seq, pack_bc);
+            });
+        }
+
         Sorter<k, true>::sort(registers);
+
+        {
+            auto mask = SimdOps::broadcast((1 << shift) - 1);
+
+            tlx::call_for_range<0, k>([&](size_t idx) {
+                registers[idx] = SimdOps::bitwise_and(registers[idx], mask);
+            });
+        }
 
         if ( !partial_size ) {
             store<k, kAligned, kStream>(packed_it, registers);
         } else {
             store<k-1, kAligned, kStream>(packed_it, registers);
             SimdOps::partial_store(packed_it + (k-1), partial_size, registers[k-1]);
+        }
+    }
+
+
+    template <size_t k, typename CallbackLoad, typename CallbackStore>
+    static void sort_callback(CallbackLoad cbl, CallbackStore cbw, size_t n) {
+        const auto partial_size = n % SimdOps::kPacking;
+
+        simd_type registers[k];
+
+        constexpr int shift = ::tlx::Log2<k * SimdOps::kPacking>::ceil;
+        {
+            auto index_seq = SimdOps::index_sequence();
+            const auto pack_bc = SimdOps::broadcast(SimdOps::kPacking);
+
+            auto add_index = [&] (simd_type x) {
+                auto tmp = SimdOps::shift_left(x, shift);
+                x = SimdOps::bitwise_or(tmp, index_seq);
+
+                index_seq = SimdOps::add(index_seq, pack_bc);
+                return x;
+            };
+
+            if (!partial_size) {
+                tlx::call_for_range<0, k>([&](size_t idx) {
+                    registers[idx] = add_index(SimdOps::load_callback([idx, &cbl](auto i) { return cbl(idx * SimdOps::kPacking + i); }));
+                });
+            } else {
+                tlx::call_for_range<0, k - 1>([&](size_t idx) {
+                    registers[idx] = add_index(SimdOps::load_callback([idx, &cbl](auto i) { return cbl(idx * SimdOps::kPacking + i); }));
+                });
+                registers[k - 1] = add_index(SimdOps::partial_load_callback([&cbl](auto i) { return cbl((k - 1) * SimdOps::kPacking + i); },
+                                                                  partial_size, std::numeric_limits<value_type>::max()));
+            }
+        }
+
+        Sorter<k, true>::sort(registers);
+
+        {
+            auto mask = SimdOps::broadcast((1 << shift) - 1);
+
+            if ( !partial_size ) {
+                tlx::call_for_range<0, k>([&] (size_t idx) {
+                    SimdOps::store_callback([&] (auto i, auto x) {cbw(idx * SimdOps::kPacking + i, x);},  SimdOps::bitwise_and(mask, registers[idx]));
+                });
+            } else {
+                tlx::call_for_range<0, k - 1>([&] (size_t idx) {
+                    SimdOps::store_callback([&] (auto i, auto x) {cbw(idx * SimdOps::kPacking + i, x);},  SimdOps::bitwise_and(mask, registers[idx]));
+                });
+                SimdOps::partial_store_callback([&] (auto i, auto x) {cbw((k-1) * SimdOps::kPacking + i, x);}, partial_size,  SimdOps::bitwise_and(mask, registers[k-1]));
+            }
         }
     }
 
